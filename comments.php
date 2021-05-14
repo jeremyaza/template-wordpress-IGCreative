02a953e1c02002cbdf4434bd6378aae78d7a2f45
Jeremy <bvbjerencript_09@hotmail.com>
2021-05-14 02:24:49 -0500

    Cambio de la estructura de la etiquetas html, para mejorar el aspecto visual

--- comments.php
+++ comments.php
@@ -32,13 +32,13 @@ if ( post_password_required() ) {
 			if ( '1' === $test_comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'test' ),
+					esc_html__( 'Un pensamiento sobre &ldquo;%1$s&rdquo;', 'test' ),
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
 			} else {
 				printf( 
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $test_comment_count, 'comments title', 'test' ) ),
+					esc_html( _nx( '%1$s pensamientos sobre &ldquo;%2$s&rdquo;', '%1$s pensamientos sobre &ldquo;%2$s&rdquo;', $test_comment_count, 'comments title', 'test' ) ),
 					number_format_i18n( $test_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
@@ -65,7 +65,7 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) :
 			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'test' ); ?></p>
+			<p class="no-comments"><?php esc_html_e( 'No hay comentarios.', 'test' ); ?></p>
 			<?php
 		endif;
 
