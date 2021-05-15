13e49df0d25d207de0a24d3e7b3137e2436f0d98
Jeremy <bvbjerencript_09@hotmail.com>
2021-05-14 22:01:56 -0500

    Cambio de de los estilos de la pagina del error 404 y del error de busqueda de las páginas

--- 404.php
+++ 404.php
@@ -13,21 +13,132 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'test' ); ?></h1>
+				<header class="page-header">
+				<div id="content_questions">
+					<div class="marks question_mark_1">
+						<svg version="1.1" id="question" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 30.4 52">
+							<g id="qmark">
+								<path
+									class="qm"
+									d="M15.8,31.1c0.5-1.5,1.1-2.9,1.9-4c0.8-1.1,1.8-2.4,3.1-3.8c1.7-1.9,3-3.6,3.8-5c0.8-1.4,1.2-3.1,1.1-5.1
+							c-0.1-2.7-1.1-4.8-3-6.4c-1.9-1.6-4.4-2.3-7.5-2.1C10,4.9,6.1,7,3.5,11.1L0,8.3c3.4-5.2,8.5-8,15.4-8.3c4.4-0.2,8,0.8,10.7,3
+							c2.7,2.2,4.2,5.2,4.3,9.1c0.1,2-0.1,3.8-0.6,5.3c-0.5,1.6-1.1,2.9-1.9,4c-0.8,1.1-1.8,2.4-3.1,3.9c-1.7,2-3,3.7-3.8,5.3
+							c-0.8,1.5-1.2,3.4-1.1,5.6l-4.7,0.2C15.1,34.4,15.3,32.6,15.8,31.1z M15.8,50.9c-0.7-0.7-1-1.6-1.1-2.6c0-1.1,0.2-2,0.8-2.7
+							c0.6-0.7,1.4-1.1,2.3-1.2c1,0,1.8,0.3,2.4,1c0.7,0.7,1,1.6,1.1,2.6c0,1-0.2,1.9-0.9,2.7c-0.6,0.8-1.4,1.2-2.3,1.2
+							C17.3,52,16.5,51.7,15.8,50.9z"
+								/>
+							</g>
+						</svg>
+					</div>
+					<div class="marks question_mark_2">
+					<svg version="1.1" id="question" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 30.4 52">
+						<g id="qmark">
+						<path
+							class="qm"
+							d="M15.8,31.1c0.5-1.5,1.1-2.9,1.9-4c0.8-1.1,1.8-2.4,3.1-3.8c1.7-1.9,3-3.6,3.8-5c0.8-1.4,1.2-3.1,1.1-5.1
+					c-0.1-2.7-1.1-4.8-3-6.4c-1.9-1.6-4.4-2.3-7.5-2.1C10,4.9,6.1,7,3.5,11.1L0,8.3c3.4-5.2,8.5-8,15.4-8.3c4.4-0.2,8,0.8,10.7,3
+					c2.7,2.2,4.2,5.2,4.3,9.1c0.1,2-0.1,3.8-0.6,5.3c-0.5,1.6-1.1,2.9-1.9,4c-0.8,1.1-1.8,2.4-3.1,3.9c-1.7,2-3,3.7-3.8,5.3
+					c-0.8,1.5-1.2,3.4-1.1,5.6l-4.7,0.2C15.1,34.4,15.3,32.6,15.8,31.1z M15.8,50.9c-0.7-0.7-1-1.6-1.1-2.6c0-1.1,0.2-2,0.8-2.7
+					c0.6-0.7,1.4-1.1,2.3-1.2c1,0,1.8,0.3,2.4,1c0.7,0.7,1,1.6,1.1,2.6c0,1-0.2,1.9-0.9,2.7c-0.6,0.8-1.4,1.2-2.3,1.2
+					C17.3,52,16.5,51.7,15.8,50.9z"
+						/>
+						</g>
+					</svg>
+					</div>
+					<div class="marks question_mark_3">
+					<svg version="1.1" id="question" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 30.4 52">
+						<g id="qmark">
+						<path
+							class="qm"
+							d="M15.8,31.1c0.5-1.5,1.1-2.9,1.9-4c0.8-1.1,1.8-2.4,3.1-3.8c1.7-1.9,3-3.6,3.8-5c0.8-1.4,1.2-3.1,1.1-5.1
+					c-0.1-2.7-1.1-4.8-3-6.4c-1.9-1.6-4.4-2.3-7.5-2.1C10,4.9,6.1,7,3.5,11.1L0,8.3c3.4-5.2,8.5-8,15.4-8.3c4.4-0.2,8,0.8,10.7,3
+					c2.7,2.2,4.2,5.2,4.3,9.1c0.1,2-0.1,3.8-0.6,5.3c-0.5,1.6-1.1,2.9-1.9,4c-0.8,1.1-1.8,2.4-3.1,3.9c-1.7,2-3,3.7-3.8,5.3
+					c-0.8,1.5-1.2,3.4-1.1,5.6l-4.7,0.2C15.1,34.4,15.3,32.6,15.8,31.1z M15.8,50.9c-0.7-0.7-1-1.6-1.1-2.6c0-1.1,0.2-2,0.8-2.7
+					c0.6-0.7,1.4-1.1,2.3-1.2c1,0,1.8,0.3,2.4,1c0.7,0.7,1,1.6,1.1,2.6c0,1-0.2,1.9-0.9,2.7c-0.6,0.8-1.4,1.2-2.3,1.2
+					C17.3,52,16.5,51.7,15.8,50.9z"
+						/>
+						</g>
+					</svg>
+					</div>
+				</div>
+				<div id="content_404">
+					<!-- 4 -->
+					<div class="number_4">
+						<svg version="1.1" id="number_four" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 193.9 333.7">
+							<g id="four">
+							<g>
+								<path
+								class="nFour"
+								d="M0,265.4c0-5.5,1.5-14.2,3.4-19.4L89.1,9.4C91,4.2,97,0,102.5,0h14.4c5.5,0,8.5,4.2,6.7,9.4l-83,233.8
+							c-1.8,5.2,1.2,9.4,6.7,9.4h136.7c5.5,0,10,4.5,10,10v9.6c0,5.5-4.5,10-10,10H10c-5.5,0-10-4.5-10-10V265.4z M133.6,147.2
+							c0-5.5,4.5-10,10-10h11.9c5.5,0,10,4.5,10,10v176.4c0,5.5-4.5,10-10,10h-11.9c-5.5,0-10-4.5-10-10V147.2z"
+								/>
+							</g>
+							</g>
+						</svg>
+					</div>
+					<!-- 0 -->
+					<div class="number_0">
+					<svg version="1.1" id="NotFound" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 338 338">
+						<style type="text/css"></style>
+						<circle id="circle_nf3" class="st0" cx="169" cy="169" r="169" />
+						<circle id="circle_nf2" class="st0" cx="169" cy="169" r="139.5" />
+						<circle id="circle_nf1" class="st1" cx="169" cy="169" r="111.5" />
+						<g id="face">
+							<ellipse class="st2" cx="126.8" cy="151.8" rx="15.9" ry="20.6" />
+							<ellipse class="st2" cx="213.3" cy="152.3" rx="15.9" ry="20.6" />
+							<path
+								class="st2"
+								d="M201.2,213.1c0,9.3-13,16.8-29,16.8s-29-7.5-29-16.8c0-8.1,9.7-14.8,22.8-16.4c2.1-0.3,4.2-0.4,6.3-0.4
+					c3.5,0,7,0.4,10.4,1.1C193.5,199.8,201.2,205.9,201.2,213.1z"
+							/>
+							<path
+								class="st3"
+								d="M182.9,198.8c0,1.2-0.4,2.3-1.2,3.2c-0.8,0.8-1.8,1.3-3,1.3h-9.4c-2.4-0.1-4.3-2.1-4.2-4.5
+					c0-0.9,0.3-1.8,0.8-2.6c2.1-0.3,4.2-0.4,6.3-0.4c3.5,0,7,0.4,10.4,1.2C182.8,197.6,182.9,198.2,182.9,198.8z"
+							/>
+							<circle class="st3" cx="130.5" cy="141.5" r="4.7" />
+							<circle class="st3" cx="217.6" cy="141.5" r="4.7" />
+							<circle class="st4" cx="222.2" cy="185.5" r="4.7" />
+							<circle class="st4" cx="108.1" cy="185.5" r="4.7" />
+						</g>
+					</svg>
+				</div>
+				<!-- 4 -->
+				<div class="number_4">
+					<svg version="1.1" id="number_four" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 193.9 333.7">
+						<g id="four">
+						<g>
+							<path
+							class="nFour"
+							d="M0,265.4c0-5.5,1.5-14.2,3.4-19.4L89.1,9.4C91,4.2,97,0,102.5,0h14.4c5.5,0,8.5,4.2,6.7,9.4l-83,233.8
+						c-1.8,5.2,1.2,9.4,6.7,9.4h136.7c5.5,0,10,4.5,10,10v9.6c0,5.5-4.5,10-10,10H10c-5.5,0-10-4.5-10-10V265.4z M133.6,147.2
+						c0-5.5,4.5-10,10-10h11.9c5.5,0,10,4.5,10,10v176.4c0,5.5-4.5,10-10,10h-11.9c-5.5,0-10-4.5-10-10V147.2z"
+							/>
+						</g>
+						</g>
+					</svg>
+				</div>
+			</div>
+			<div class="info_not_found">
+				<h2 class="page-title"><?php esc_html_e( 'Oops! La página que buscas no se encuentra', 'test' ); ?></h2>
+				<p><?php esc_html_e( 'Parece que no se ha encontrado nada en este lugar. ¿Tal vez probar uno de los enlaces de abajo o una búsqueda?', 'test' ); ?></p>
+
+				<?php 
+					get_search_form();
+				?>
+			</div>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'test' ); ?></p>
 
 					<?php
-					get_search_form();
 
 					the_widget( 'WP_Widget_Recent_Posts' );
 					?>
 
 					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'test' ); ?></h2>
+						<h2 class="widget-title"><?php esc_html_e( 'Categorias mas usadas', 'test' ); ?></h2>
 						<ul>
 							<?php
 							wp_list_categories(
@@ -45,7 +156,7 @@ get_header();
 
 					<?php
 					/* translators: %1$s: smiley */
-					$test_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'test' ), convert_smilies( ':)' ) ) . '</p>';
+					$test_archive_content = '<p>' . sprintf( esc_html__( 'Intenta buscar en los archivos mensuales. %1$s', 'test' ), '' ) . '</p>';
 					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$test_archive_content" );
 
 					the_widget( 'WP_Widget_Tag_Cloud' );
