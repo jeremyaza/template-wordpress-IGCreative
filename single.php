d0f423b0f74b7ee713ded8439f9a1f4027bb30d2
Jeremy <bvbjerencript_09@hotmail.com>
2021-05-14 21:59:26 -0500

    Estilos del carrusel de next y previous para las entradas

--- single.php
+++ single.php
@@ -18,6 +18,11 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
 			the_post_navigation(
 				array(
 					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'test' ) . '</span> <span class="nav-title">%title</span>',
@@ -25,10 +30,6 @@ get_header();
 				)
 			);
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
 
 		endwhile; // End of the loop.
 		?>
